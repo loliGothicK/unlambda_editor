67b236b2c2bb19f6af6f82e1934db7a131472229
mitama <loligothick@gmail.com>
2019-02-17 15:52:24 +0900

    polymorphism safety improvements

--- fnc.h
+++ fnc.h
@@ -13,18 +13,19 @@ public:
 	Fnc *insert_left(), *insert_right();
 	Fnc *com(char), *app(char, char);
 	void show(Fnc *);
+	virtual ~Fnc() {}
 };
 
-class Com : public Fnc{
+class Com final : public Fnc{
 	const char text;
 	void _show(Fnc *);
 	~Com();
 public:
 	Com(Fnc *, Fnc **, char);
-	Fnc *child_left(), *child_right();
+	virtual Fnc *child_left(), *child_right() override final;
 };
 
-class App : public Fnc{
+class App final : public Fnc{
 	Fnc *lft, *rgt;
 	void _show(Fnc *);
 	~App();
